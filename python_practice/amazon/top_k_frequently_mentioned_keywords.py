a17413f62d1af8d0edb5ee1d195e8e752cd1b93e
Weikun Han <weikunhan@g.ucla.edu>
2020-03-24 21:05:49 -0700

    new

---
+++ python_practice/amazon/top_k_frequently_mentioned_keywords.py
@@ -0,0 +1,89 @@
+""" Top K Frequently Mentioned Keywords
+
+Given a list of reviews, a list of keywords and an integer k. 
+Find the most popular k keywords in order of most to least frequently mentioned.
+The comparison of strings is case-insensitive. If keywords are mentioned an 
+equal number of times in reviews, sort alphabetically.
+
+Author: Weikun Han <weikunhan@g.ucla.edu>
+
+Reference: https://leetcode.com/discuss/interview-question/542597/
+
+Example 1:
+Input:
+k = 2
+keywords = ["anacell", "cetracular", "betacellular"]
+reviews = [
+  "Anacell provides the best services in the city",
+  "betacellular has awesome services",
+  "Best services provided by anacell, everyone should use anacell",
+]
+Output:
+["anacell", "betacellular"]
+Explanation:
+"anacell" is occuring in 2 different reviews and "betacellular" is only occuring 
+in 1 review.
+
+Example 2:
+Input:
+k = 2
+keywords = ["anacell", "betacellular", "cetracular", "deltacellular", "eurocell"]
+reviews = [
+  "I love anacell Best services; Best services provided by anacell",
+  "betacellular has great services",
+  "deltacellular provides much better services than betacellular",
+  "cetracular is worse than anacell",
+  "Betacellular is better than deltacellular.",
+]
+Output:
+["betacellular", "anacell"]
+Explanation:
+"betacellular" is occuring in 3 different reviews. "anacell" and "deltacellular" 
+are occuring in 2 reviews, but "anacell" is lexicographically smaller.
+"""
+
+import collections
+import heapq
+import re
+
+class Solution(object):
+    def topKFrequentMentionedKeywords(self, keywords, reviews, k):
+        """
+        :type keywords: List[str]
+        :type reviews: List[str]
+        :type k: int
+        :rtype: List[str]
+        """
+
+        data_value_dict = collections.Counter()
+        key_value_dict = set(keywords)
+        res = []
+
+        for review in reviews:
+            temp_list = review.lower().split(' ')
+
+            for value in temp_list:
+                value = re.sub('[^a-z]', '', value)
+
+                if value in key_value_dict:
+                    data_value_dict[value] += 1
+
+        res = heapq.nsmallest(k, data_value_dict, key=lambda x: (-data_value_dict[x], x))
+
+        return res
+
+def main():
+    k = 2
+    keywords = ["anacell", "betacellular", "cetracular", "deltacellular", "eurocell"]
+    reviews = [
+        "I love anacell Best services; Best services provided by anacell",
+        "betacellular has great services",
+        "deltacellular provides much better services than betacellular",
+        "cetracular is worse than anacell",
+        "Betacellular is better than deltacellular."]
+    solution = Solution()
+    res = solution.topKFrequentMentionedKeywords(keywords, reviews, k)
+    print(res)
+
+if __name__ == "__main__": 
+    main()
