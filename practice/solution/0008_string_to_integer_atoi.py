562b70af90e27aa49bbb188da81a758f4a03612a
Weikun Han <weikunhan@g.ucla.edu>
2020-06-28 20:52:17 -0700

    add 008

---
+++ practice/solution/0008_string_to_integer_atoi.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def myAtoi(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+        
+        sign = 1
+        count = 0
+        res = 0
+        temp_value = str.strip()
+        
+        if not temp_value:
+            
+            return res
+        
+        if temp_value[0] == '-':
+            sign = -1
+            temp_value = temp_value[1:]
+        elif temp_value[0] == '+':
+            temp_value = temp_value[1:]
+        
+        while count < len(temp_value) and temp_value[count].isdigit():
+            res = res * 10 + int(temp_value[count])
+            count += 1
+        
+        res = max(-2**31, min(sign * res, 2**31 - 1))
+            
+        return res
+            
