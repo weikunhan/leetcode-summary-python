d39d97544d8ea3d1aa8d5e59f067eb5bf3663e9c
Weikun Han <weikunhan@g.ucla.edu>
2020-09-03 22:40:01 -0700

    update

---
+++ practice/solution/0056_merge_intervals.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        
+        value_list = sorted(intervals)
+        res = []
+        
+        if not intervals:
+            
+            return res
+        
+        res = [value_list[0]]
+        
+        for interval in value_list[1:]:
+            if res[-1][1] >= interval[0]:
+                res[-1][1] = max(res[-1][1], interval[1])
+            else:
+                res.append(interval)
+
+        return res
