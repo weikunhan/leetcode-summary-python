c61ece1e60b5e44c9cfc68c69c6d9acbe639da38
Weikun Han <weikunhan@g.ucla.edu>
2020-09-06 22:32:59 -0700

    update

---
+++ practice/solution/0277_find_the_celebrity.py
@@ -0,0 +1,24 @@
+# The knows API is already defined for you.
+# @param a, person a
+# @param b, person b
+# @return a boolean, whether a knows b
+# def knows(a, b):
+
+class Solution(object):
+    def findCelebrity(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        res = 0
+        
+        for i in range(1, n):
+            if knows(res, i):
+                res = i
+                
+        for i in range(n):
+            if not knows(i, res) or (i != res and knows(res, i)):
+                res = -1
+                                    
+        return res
