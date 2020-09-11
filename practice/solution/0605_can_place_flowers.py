99a5369a129705073cea1aab9b37cfadd5b544f5
Weikun Han <weikunhan@g.ucla.edu>
2020-09-10 23:36:07 -0700

    add

--- practice/solution/0605_can_place_flowers.py
+++ practice/solution/0605_can_place_flowers.py
@@ -6,12 +6,12 @@ class Solution(object):
         :rtype: bool
         """
         count = 0
-        flowerbed = [0] + flowerbed + [0]
+        value_list = [0] + flowerbed + [0]
         res = False
         
-        for i in range(1, len(flowerbed) - 1):
-        	if flowerbed[i - 1] == flowerbed[i] == flowerbed[i + 1] == 0:
-        		flowerbed[i] = 1
+        for i in range(1, len(value_list) - 1):
+        	if value_list[i - 1] == value_list[i] == value_list[i + 1] == 0:
+        		value_list[i] = 1
         		count += 1
                 
         if count >= n:
