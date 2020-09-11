5cf500b15d09eef325d87263286a4d81bc81e2c7
Weikun Han <weikunhan@g.ucla.edu>
2020-09-10 22:56:19 -0700

    update

---
+++ practice/solution/0605_can_place_flowers.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """
+        count = 0
+        flowerbed = [0] + flowerbed + [0]
+        res = False
+        
+        for i in range(1, len(flowerbed) - 1):
+        	if flowerbed[i - 1] == flowerbed[i] == flowerbed[i + 1] == 0:
+        		flowerbed[i] = 1
+        		count += 1
+                
+        if count >= n:
+            res = True
+            
+        return res 
