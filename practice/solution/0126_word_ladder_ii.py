fcc4f3dc9a297d5b567c3dfeed01454f40584663
Weikun Han <weikunhan@g.ucla.edu>
2020-10-20 10:33:26 -0700

    update

--- practice/solution/0126_word_ladder_ii.py
+++ practice/solution/0126_word_ladder_ii.py
@@ -10,14 +10,14 @@ class Solution(object):
         """
         
         value_dict = set(wordList)
-        visit_value_dict = {beginWord:[[beginWord]]}
+        visit_value_dict = {beginWord: [[beginWord]]}
         res = []
         
         while visit_value_dict:
             temp_value_dict = collections.defaultdict(list)
             
             for word, nested_value_list in visit_value_dict.items():
-                if word == endWord:
+                if endWord == word:
                     res = nested_value_list
                     
                     return res
@@ -25,12 +25,12 @@ class Solution(object):
                 for i in range(len(word)):
                     for char in 'abcdefghijklmnopqrstuvwxyz':
                         new_word = word[:i] + char + word[i + 1:]
-
+                        
                         if not new_word in visit_value_dict and new_word in value_dict:
                             for value_list in nested_value_list:
                                 temp_value_dict[new_word] += [value_list + [new_word]]
                                 
             value_dict -= set(temp_value_dict.keys())
             visit_value_dict = temp_value_dict
-                            
+            
         return res
