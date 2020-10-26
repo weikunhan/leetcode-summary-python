87242bb768c0b1b1d79e96cde20463a5d82696f2
Weikun Han <weikunhan@g.ucla.edu>
2020-10-25 23:33:12 -0700

    update

--- practice/solution/0103_binary_tree_zigzag_level_order_traversal.py
+++ practice/solution/0103_binary_tree_zigzag_level_order_traversal.py
@@ -1,11 +1,12 @@
-import collections
-
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+import collections
+
 class Solution(object):
     def zigzagLevelOrder(self, root):
         """
--- practice/solution/0107_binary_tree_level_order_traversal_ii.py
+++ practice/solution/0107_binary_tree_level_order_traversal_ii.py
@@ -1,11 +1,11 @@
-import collections
-
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
+
+import collections
 
 class Solution(object):
     def levelOrderBottom(self, root):
