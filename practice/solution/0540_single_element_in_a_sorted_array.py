916143e0c79ae618b043b0b20d6ca7ccb16f2491
Weikun Han <weikunhan@g.ucla.edu>
2020-11-15 19:53:59 -0800

    udpat e

--- practice/solution/0540_single_element_in_a_sorted_array.py
+++ practice/solution/0540_single_element_in_a_sorted_array.py
@@ -12,7 +12,7 @@ class Solution(object):
         while low < high:
             mid = 2 * ((low + high) // 4)
 
-            if nums[mid ] == nums[mid + 1]:
+            if nums[mid] == nums[mid + 1]:
                 low = mid + 2
             else:
                 high = mid
