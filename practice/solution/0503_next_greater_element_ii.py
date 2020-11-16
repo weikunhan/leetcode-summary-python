eaab7421ad88540bc9390e2a8435ec5fbd5de53b
Weikun Han <weikunhan@g.ucla.edu>
2020-11-15 20:23:33 -0800

    update

--- practice/solution/0503_next_greater_element_ii.py
+++ practice/solution/0503_next_greater_element_ii.py
@@ -4,22 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-
+        
         value_stack = []
         res = [-1] * len(nums)
+        
+        for _ in range(2):
+            for i in range(len(nums)):
+                while value_stack and nums[value_stack[-1]] < nums[i]:
+                    temp_value = value_stack.pop()
+                    res[temp_value] = nums[i]
 
-        for i in range(len(nums)):
-            while value_stack and nums[value_stack[-1]] < nums[i]:
-                temp_value = value_stack.pop()
-                res[temp_value] = nums[i]
-
-            value_stack.append(i)
-
-        for i in range(len(nums)):
-            while value_stack and nums[value_stack[-1]] < nums[i]:
-                temp_value = value_stack.pop()
-                res[temp_value] = nums[i]
-
-            value_stack.append(i)
-
+                value_stack.append(i)
+                
         return res
