d7347414ca47abe2cc5b8e701cb5229ca54a4e4f
Weikun Han <weikunhan@g.ucla.edu>
2020-11-17 22:04:28 -0800

    updat e

--- practice/solution/0027_remove_element.py
+++ practice/solution/0027_remove_element.py
@@ -13,7 +13,7 @@ class Solution(object):
             if nums[i] == val:
                 count += 1
             else: 
-                nums[i - count] = nums[i];
+                nums[i - count] = nums[i]
         
         res = len(nums) - count
         
