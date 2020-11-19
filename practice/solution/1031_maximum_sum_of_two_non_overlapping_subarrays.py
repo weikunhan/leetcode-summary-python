1c048d189c7e24467f3b7c07643cf3ac0f3fa6a7
Weikun Han <weikunhan@g.ucla.edu>
2020-11-18 17:11:56 -0800

    udpate

--- practice/solution/1031_maximum_sum_of_two_non_overlapping_subarrays.py
+++ practice/solution/1031_maximum_sum_of_two_non_overlapping_subarrays.py
@@ -8,20 +8,20 @@ class Solution(object):
         """
         
         value_list = A
-        right_sum_value = 0
-        left_sum_value = 0
+        right_value = 0
+        left_value = 0
         res = 0
         
         for i in range(1, len(A)):
             value_list[i] += value_list[i - 1]
         
-        left_sum_value = value_list[L - 1]
-        right_sum_value = value_list[M - 1]
+        left_value = value_list[L - 1]
+        right_value = value_list[M - 1]
         res = value_list[L + M - 1]
         
         for i in range(L + M, len(value_list)):
-            left_sum_value = max(left_sum_value, value_list[i - M] - value_list[i - L - M])
-            right_sum_value = max(right_sum_value, value_list[i - L] - value_list[i - L - M])
-            res = max(res, left_sum_value + value_list[i] - value_list[i - M], right_sum_value + value_list[i] - value_list[i - L])
+            left_value = max(left_value, value_list[i - M] - value_list[i - L - M])
+            right_value = max(right_value, value_list[i - L] - value_list[i - L - M])
+            res = max(res, left_value + value_list[i] - value_list[i - M], right_value + value_list[i] - value_list[i - L])
             
         return res
