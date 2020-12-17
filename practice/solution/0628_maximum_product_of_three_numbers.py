9b747c570fcf202a68412271aa8d4a771a8fd3b2
Weikun Han <weikunhan@g.ucla.edu>
2020-12-16 17:36:42 -0800

    update 0628

--- practice/solution/0628_maximum_product_of_three_numbers.py
+++ practice/solution/0628_maximum_product_of_three_numbers.py
@@ -6,8 +6,6 @@ class Solution(object):
         """
         
         value_list = sorted(nums)
-        res = 0
-        
         res = max(value_list[-1] * value_list[-2] * value_list[-3], value_list[0] * value_list[1] * value_list[-1])
         
         return res
