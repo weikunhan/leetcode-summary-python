45e4efe14e885894e4e5210a17c19400ea4dbc6b
Weikun Han <weikunhan@g.ucla.edu>
2020-12-17 16:48:59 -0800

    udpate 253

--- practice/solution/0253_meeting_rooms_ii.py
+++ practice/solution/0253_meeting_rooms_ii.py
@@ -13,8 +13,8 @@ class Solution(object):
             value_list.append((interval[0], 1))
             value_list.append((interval[1], -1))
         
-        for value in sorted(value_list):
-            temp_value += value[1]
+        for time in sorted(value_list):
+            temp_value += time[1]
             res = max(res, temp_value)
             
         return res
