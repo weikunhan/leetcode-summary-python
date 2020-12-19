4fd7b76bf32c8a415e68149622c59e544f0aa984
Weikun Han <weikunhan@g.ucla.edu>
2020-12-18 20:53:29 -0800

    update 1242

--- practice/solution/1242_web_crawler_multithreaded.py
+++ practice/solution/1242_web_crawler_multithreaded.py
@@ -21,35 +21,35 @@ class Solution(object):
         """
         
         self.value_list = collections.deque([startUrl])
-        self.hostname_value = 'http://' + startUrl.split('/')[2]
         self.work_value = 10
         self.thread_lock = threading.Lock()
+        self.hostname_value = 'http://' + startUrl.split('/')[2]
         self.value_dict = set(self.value_list)
         self.res = []
         
         while self.value_list:
             temp_value = len(self.value_list)
             temp_list = []
-            count = 0
+            count = 1
             
             for _ in range(temp_value):
                 url = self.value_list.popleft()
-                thread = threading.Thread(target=self.helper, args=(url, htmlParser))
-                temp_list.append(thread)
+                thread_value = threading.Thread(target=self.helper, args=(url, htmlParser))
+                temp_list.append(thread_value)
                 count += 1
                 
                 if count == self.work_value:
                     self.executor(temp_list)
-                    count = 0
                     temp_list = []
+                    count = 1
                     
             if temp_list:
                 self.executor(temp_list)
-        
+                    
         self.res = list(self.value_dict)
         
         return self.res
-                
+            
     def helper(self, url, htmlParser):
         for sub_url in htmlParser.getUrls(url):
             if not sub_url in self.value_dict and self.hostname_value in sub_url:
@@ -57,7 +57,7 @@ class Solution(object):
                 self.value_list.append(sub_url)
                 self.value_dict.add(sub_url)
                 self.thread_lock.release()
-                
+        
     def executor(self, thread_value_list):
         for thread in thread_value_list:
             thread.start()
