07d62c618544a85bb5e6ff163e2ff804bda70352
Weikun Han <weikunhan@g.ucla.edu>
2020-12-22 17:24:09 -0800

    update 91

--- practice/solution/0091_decode_ways.py
+++ practice/solution/0091_decode_ways.py
@@ -4,22 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-
+        
         end = len(s)
         dp_list = [0] * (end + 1)
         dp_list[0] = 1
         res = 0
-
+        
         if s[0] != '0':
             dp_list[1] = 1
-
-        for i in range(2, len(dp_list)):
+            
+        for i in range(2, len(s) + 1):
             if s[i - 1:i] != '0':
                 dp_list[i] += dp_list[i - 1]
-
+                
             if 10 <= int(s[i - 2:i]) <= 26:
                 dp_list[i] += dp_list[i - 2]
-
+                
         res = dp_list[-1]
-
+        
         return res
+            
