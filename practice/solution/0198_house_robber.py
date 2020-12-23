9a5ef40599e860cb17b5d530240532168bae0666
Weikun Han <weikunhan@g.ucla.edu>
2020-12-22 17:01:49 -0800

    udpate 198

--- practice/solution/0198_house_robber.py
+++ practice/solution/0198_house_robber.py
@@ -8,15 +8,15 @@ class Solution(object):
         dp_list = nums
         res = 0
         
-        if not dp_list:
+        if not nums:
             
             return res
         
-        if len(dp_list) > 1:
-            dp_list[1] = max(dp_list[0], dp_list[1])
-            
-        for i in range(2, len(dp_list)):
-            dp_list[i] = max(dp_list[i] + dp_list[i - 2], dp_list[i - 1])
+        if len(nums) > 1:
+            dp_list[1] = max(dp_list[1], dp_list[0])
+        
+        for i in range(2, len(nums)):
+            dp_list[i] = max(dp_list[i - 1], dp_list[i] + dp_list[i - 2])
             
         res = dp_list[-1]
         
