248d0f0214e808648966edf1819e5e7f1f4d10d2
Weikun Han <weikunhan@g.ucla.edu>
2020-12-24 18:40:13 -0800

    udpate 53

--- practice/solution/0053_maximum_subarray.py
+++ practice/solution/0053_maximum_subarray.py
@@ -10,9 +10,9 @@ class Solution(object):
         dp_list[0] = nums[0]
         res = 0
         
-        for i in range(1, len(dp_list)):
+        for i in range(1, len(nums)):
             dp_list[i] = max(dp_list[i - 1] + nums[i], nums[i])
-                    
-        res = max(dp_list)            
+            
+        res = max(dp_list)
         
         return res
