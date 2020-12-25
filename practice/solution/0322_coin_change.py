5e79010908cfc41cca34756a933a45845039d3fd
Weikun Han <weikunhan@g.ucla.edu>
2020-12-24 17:56:31 -0800

    update 322

--- practice/solution/0322_coin_change.py
+++ practice/solution/0322_coin_change.py
@@ -7,15 +7,15 @@ class Solution(object):
         """
         
         end = amount
-        dp_list = [end + 1] * (end + 1)
+        dp_list = [amount + 1] * (end + 1)
         dp_list[0] = 0
         res = -1
         
         for coin in coins:
-            for i in range(coin, len(dp_list)):
+            for i in range(coin, amount + 1):
                 dp_list[i] = min(dp_list[i], dp_list[i - coin] + 1)
                 
-        if dp_list[-1] != end + 1:
+        if dp_list[-1] != amount + 1:
             res = dp_list[-1]
-        
-        return  res
+            
+        return res
