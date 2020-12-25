21702441c215150270434277a07b3b32820eb36a
Weikun Han <weikunhan@g.ucla.edu>
2020-12-24 17:21:02 -0800

    udpate 256

--- practice/solution/0256_paint_house.py
+++ practice/solution/0256_paint_house.py
@@ -14,7 +14,7 @@ class Solution(object):
         
         for i in range(1, len(costs)):
             for j in range(3):
-                dp_list[i][j] += min(dp_list[i - 1][(j + 1) % 3], dp_list[i - 1][(j + 2) % 3])
+                dp_list[i][j] += min(dp_list[i - 1][(j + 1) % 3], dp_list[i - 1][(j - 1) % 3])
         
         
         res = min(min(dp_list[-1][0], dp_list[-1][1]), dp_list[-1][2])
