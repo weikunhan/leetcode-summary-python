78648e70a0215fcd9c47f50a0c904830ad2c8a65
Weikun Han <weikunhan@g.ucla.edu>
2020-12-26 10:54:36 -0800

    update 167

--- practice/solution/0167_two_sum_ii_input_array_is_sorted.py
+++ practice/solution/0167_two_sum_ii_input_array_is_sorted.py
@@ -12,12 +12,12 @@ class Solution(object):
         for i in range(len(numbers)):
             temp_value = target - numbers[i]
             
-            if not numbers[i] in value_dict:
-                value_dict[temp_value] = i
-            else:
-                res.append(value_dict[numbers[i]] + 1)
-                res.append(i + 1)
+            if numbers[i] in value_dict:
+                res = [value_dict[numbers[i]] + 1, i + 1]
                 
                 return res
+            else:
+                value_dict[temp_value] = i
                 
         return res
+
