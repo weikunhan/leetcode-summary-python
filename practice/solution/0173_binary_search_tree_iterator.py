a3e1501f8926bcb7b84d77d875a72c767596102b
Weikun Han <weikunhan@g.ucla.edu>
2020-12-26 19:40:08 -0800

    update 173

--- practice/solution/0173_binary_search_tree_iterator.py
+++ practice/solution/0173_binary_search_tree_iterator.py
@@ -11,42 +11,40 @@ class BSTIterator(object):
         """
         :type root: TreeNode
         """
-
-        self.value_stack = []
-
-        while root:
-            self.value_stack.append(root)
-            root = root.left        
-
-    def next(self):
-        """
-        @return the next smallest number
-        :rtype: int
-        """
         
-        root = self.value_stack.pop()
-        temp_value = root.val
-        root = root.right
-
+        self.value_stack = []
+        
         while root:
             self.value_stack.append(root)
             root = root.left
 
-        return temp_value
+    def next(self):
+        """
+        :rtype: int
+        """
+        
+        temp_root = self.value_stack.pop()
+        temp_value = temp_root.val
+        temp_root = temp_root.right
+        
+        while temp_root:
+            self.value_stack.append(temp_root)
+            temp_root = temp_root.left
+            
+        return temp_value        
 
     def hasNext(self):
         """
-        @return whether we have a next smallest number
         :rtype: bool
         """
-
+        
         if self.value_stack:
-
+            
             return True
         else:
-
+            
             return False
-        
+
 
 # Your BSTIterator object will be instantiated and called as such:
 # obj = BSTIterator(root)
