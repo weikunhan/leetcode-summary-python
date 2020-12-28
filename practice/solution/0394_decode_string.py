38eaf72a2f97d9a6a341c20cec488025b657105b
Weikun Han <weikunhan@g.ucla.edu>
2020-12-27 17:08:23 -0800

    update 394

--- practice/solution/0394_decode_string.py
+++ practice/solution/0394_decode_string.py
@@ -7,24 +7,22 @@ class Solution(object):
         
         value_stack = []
         sum_value = 0
-        pre_value = ''
+        word_value = ''
         res = ''
         
         for char in s:
-            if char =='[':
-                value_stack.append(pre_value)
-                value_stack.append(sum_value)
-                pre_value = ''
+            if char == '[':
+                value_stack.append((word_value, sum_value))
+                word_value = ''
                 sum_value = 0
             elif char == ']':
-                right_value = value_stack.pop()
-                left_value = value_stack.pop()
-                pre_value = left_value + pre_value * right_value
+                temp_value, num_value = value_stack.pop()
+                word_value = temp_value + word_value * num_value
             elif char.isdigit():
                 sum_value = sum_value * 10 + int(char)
             else:
-                pre_value += char
-            
-        res = pre_value    
-            
+                word_value += char
+                
+        res = word_value
+        
         return res
