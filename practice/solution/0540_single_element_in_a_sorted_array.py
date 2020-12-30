74c8085379f29b35cd5b440b01ae2be273f5b91e
Weikun Han <weikunhan@g.ucla.edu>
2020-12-29 16:19:57 -0800

    update 540

--- practice/solution/0540_single_element_in_a_sorted_array.py
+++ practice/solution/0540_single_element_in_a_sorted_array.py
@@ -4,19 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+
         low = 0
         high = len(nums) - 1
-        res = 0 
-
+        res = 0
+        
         while low < high:
             mid = 2 * ((low + high) // 4)
-
+            
             if nums[mid] == nums[mid + 1]:
                 low = mid + 2
             else:
                 high = mid
-
+                
         res = nums[low]
-
+        
         return res
