dafb575358c26199bf0646c4743351c50700b08f
Weikun Han <weikunhan@g.ucla.edu>
2021-01-05 11:59:00 -0800

    update 819

--- practice/solution/0819_most_common_word.py
+++ practice/solution/0819_most_common_word.py
@@ -1,5 +1,5 @@
-import heapq
 import collections
+import heapq
 
 class Solution(object):
     def mostCommonWord(self, paragraph, banned):
@@ -9,16 +9,16 @@ class Solution(object):
         :rtype: str
         """
         
-        value_list = re.split(r'[,\s]\s*', paragraph.lower())
-        banned_value_dict = set(banned)
+        value_list = re.split(r'[,\s]', paragraph.lower())
+        data_value_dict = set(banned)
         count_value_dict = collections.Counter()
         res = ''
         
         for word in value_list:
-            word = re.sub(r'[^a-z0-9]', '', word)
+            word_value = re.sub(r'[^a-z0-9]', '', word)
             
-            if not word in banned_value_dict:
-                count_value_dict[word] += 1
+            if word_value and not word_value in data_value_dict:
+                count_value_dict[word_value] += 1
                 
         res = heapq.nlargest(1, count_value_dict, lambda x:count_value_dict[x])[0]
         
