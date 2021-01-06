b95242541d1f4bd0725612cf797d82e55d30d1d0
Weikun Han <weikunhan@g.ucla.edu>
2021-01-06 08:53:43 -0800

    update 1212

--- practice/solution/1212_team_scores_in_football_tournament.sql
+++ practice/solution/1212_team_scores_in_football_tournament.sql
@@ -3,26 +3,13 @@
 SELECT team_id, team_name,
 SUM(CASE 
         WHEN team_id=host_team 
-        AND host_goals>guest_goals 
-        THEN 3 
-        ELSE 0 
-    END)+
-SUM(CASE 
+        AND host_goals>guest_goals THEN 3 
         WHEN team_id=guest_team 
-        AND guest_goals>host_goals 
-        THEN 3 
-        ELSE 0 
-    END)+
-SUM(CASE 
+        AND guest_goals>host_goals THEN 3 
         WHEN team_id=host_team 
-        AND host_goals=guest_goals 
-        THEN 1 
-        ELSE 0 
-    END)+
-SUM(CASE 
+        AND host_goals=guest_goals THEN 1 
         WHEN team_id=guest_team 
-        AND guest_goals=host_goals 
-        THEN 1 
+        AND guest_goals=host_goals THEN 1 
         ELSE 0 
     END) AS num_points
 FROM Teams
