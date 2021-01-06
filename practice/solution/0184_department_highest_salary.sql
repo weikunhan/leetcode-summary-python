00818f811b0f71b1aba31131e7a56d2ce7cbd62a
Weikun Han <weikunhan@g.ucla.edu>
2021-01-06 07:59:28 -0800

    update 184 old

--- practice/solution/0184_department_highest_salary.sql
+++ practice/solution/0184_department_highest_salary.sql
@@ -2,7 +2,7 @@
 
 SELECT Department.Name AS Department, Employee.Name AS Employee, Salary
 FROM Employee
-JOIN Department 
+INNER JOIN Department 
 ON Employee.DepartmentId=Department.Id 
 WHERE (DepartmentId, Salary) 
 IN (SELECT DepartmentId, MAX(Salary)
