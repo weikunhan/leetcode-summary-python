9486926e4d72a6aecd0302643d005cc522ab8360
Weikun Han <weikunhan@g.ucla.edu>
2021-01-06 08:25:26 -0800

    update 184

--- practice/solution/0184_department_highest_salary.sql
+++ practice/solution/0184_department_highest_salary.sql
@@ -2,9 +2,9 @@
 
 SELECT Department.Name AS Department, Employee.Name AS Employee, Salary
 FROM Employee
-INNER JOIN Department 
-ON Employee.DepartmentId=Department.Id 
-WHERE (DepartmentId, Salary) 
-IN (SELECT DepartmentId, MAX(Salary)
-    FROM Employee
-    GROUP BY DepartmentId)
+    INNER JOIN Department 
+    ON Employee.DepartmentId=Department.Id 
+WHERE (DepartmentId, Salary) IN 
+    (SELECT DepartmentId, MAX(Salary)
+     FROM Employee
+     GROUP BY DepartmentId)
